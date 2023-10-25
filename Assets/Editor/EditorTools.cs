c34afc83352735e15cd82e68cfab0f72cb19da12
milan.basaric <milanbasaric92@gmail.com>
2023-10-25 09:37:00 +0200

    camera quick fix

---
+++ Assets/Editor/EditorTools.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class EditorTools {
+    [MenuItem("TankSurvival/Select Camera")]
+    static void SelectCamera() {
+        Selection.activeGameObject = Camera.main.gameObject;
+    }
+}
